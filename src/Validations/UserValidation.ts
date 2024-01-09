0f4b8660bbab01af46b11a799d9720d8d9e99320
ViniiR <cvrm2004@gmail.com>
2024-01-08 23:15:50 -0300

    initial commit

--- src/Validations/UserValidation.ts
+++
@@ -1,8 +0,0 @@
-import {string, object } from 'yup'
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-export const userSchema = object().shape({
-    name: string().required("Nome é obrigatório"),
-    email: string()/*.email("Email inválido")*/.matches(emailRegex, "email inválido").required("Email é obrigatório"),
-    password: string().min(8, "Mínimo de 8 caracteres").max(32, "Máximo de 32 caracteres").required("Senha obrigatória"),
-});
--- src/config.ts
+++
@@ -1,3 +0,0 @@
-const ServerURL = "http://localhost:5000/";
-
-export default ServerURL;
